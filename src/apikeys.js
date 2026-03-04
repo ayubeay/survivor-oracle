3ebc4291b5498ef337ed7b1e5891017b77c399b4
ayubeay <ayubeay@yahoo.com>
2026-03-04 09:31:35 -0500

    clean: remove stale WAL SIGTERM handlers

--- src/apikeys.js
+++ src/apikeys.js
@@ -258,14 +258,3 @@ module.exports = {
   mountAdminRoutes,
   checkAndConsume,
 };
-
-
-// Flush WAL on shutdown so keys persist across deploys
-process.on("SIGTERM", function() {
-  try {
-    db.pragma("wal_checkpoint(TRUNCATE)");
-    console.log("[apikeys] WAL checkpoint done");
-    db.close();
-  } catch(e) { console.error("[apikeys] shutdown err:", e.message); }
-  process.exit(0);
-});
